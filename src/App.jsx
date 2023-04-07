fe6cc4459043a39f954f430aef729fb188c0fa43
Girlesa Quintero <girlesa.quintero+DFIN@dfinsolutions.com>
2023-04-07 15:33:30 -0500

    routes implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-
-import Footer from "./Components/Footer";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+
+    <>
+      <Navbar />
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contacto" element={<Contact />} />
+          <Route path="dentista/:id" element={<Detail />} />
+          <Route path="favs" element={<Favs />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+
+      <Footer />
+    </>
   );
 }
 
