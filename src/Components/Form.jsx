6a19658e86dec725fb589782c31d0873589d7760
Girlesa Quintero <girlesa.quintero+DFIN@dfinsolutions.com>
2023-04-07 20:53:04 -0500

    el mensajito de gracias al form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -7,52 +7,53 @@ const Form = () => {
   const [form, setForm] = useState({
     fullName: "",
     email: ""
-})
+  })
 
-const { fullName, email } = form
+  const { fullName, email } = form
 
-const [show, setShow] = useState(false);
-const [err, setErr] = useState(false);
+  const [show, setShow] = useState(false);
+  const [err, setErr] = useState(false);
 
-const onChangeInput = ({ target }) => {
+  const onChangeInput = ({ target }) => {
     const { name, value } = target
     setForm({
-        ...form,
-        [name]: value
+      ...form,
+      [name]: value
     })
-}
+  }
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
-    if (fullName.trim().length >= 3 && email.trim().includes("@")) {
-        setShow(true)
-        setErr(false)
-        event.target.reset()
+    if (fullName.trim().length >= 5 && email.trim().includes("@")) {
+      setShow(true)
+      setErr(false)
+      event.target.reset()
     } else {
-        setShow(false)
-        setErr(true)
+      setShow(false)
+      setErr(true)
     }
-}
+  }
 
-return (
+  return (
     <div>
-        <form onSubmit={handleSubmit} >
-            <div>
-                <label>Nombre Completo</label>
-                <input type="text" onChange={onChangeInput} name="fullName"/>
-            </div>
-            <div>
-                <label>E-mail</label>
-                <input type="email" onChange={onChangeInput} name="email"/>
-            </div>
-            <div>
-                <button type="submit">Enviar</button>
-            </div>
-        </form>
+      <form onSubmit={handleSubmit} >
+        <div>
+          <label>Nombre Completo</label>
+          <input type="text" onChange={onChangeInput} name="fullName" />
+        </div>
+        <div>
+          <label>E-mail</label>
+          <input type="email" onChange={onChangeInput} name="email" />
+        </div>
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
 
-        {err ? <p>Debes completar todo el formulario</p> : null}
+      {err ? <p>Por favor verifique su información nuevamente</p> : null}
+      {show && <p>Gracias {fullName}, te contactaremos vía email</p>}
     </div>
-)
+  )
 };
 
 export default Form;
